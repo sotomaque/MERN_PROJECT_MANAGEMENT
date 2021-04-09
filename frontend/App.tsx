9a3d7e97b083b28d4571b9d08f3eac9b14ea5102
Enrique <sotomaque@gmail.com>
2021-04-09 15:00:09 -0700

    update gitignore

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import useCachedResources from './src/hooks/useCachedResources';
-import useColorScheme from './src//hooks/useColorScheme';
-import Navigation from './src//navigation';
+import useColorScheme from './src/hooks/useColorScheme';
+import Navigation from './src/navigation';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
